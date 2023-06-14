14af842f0bddbb52c7a446fae670cd5837e4897f
Jmestrelozano <jmestrelozano@gmail.com>
2023-06-14 15:11:21 -0500

    adding modal states to my store

--- app/interfaces/store/store.ts
+++ app/interfaces/store/store.ts
@@ -1,8 +1,11 @@
 export interface Store {
   isOpenRegisterModal: boolean;
   isOpenLoginModal: boolean;
+  isOpenRentModal: boolean;
   onOpenRegisterModal: () => void;
   onCloseRegisterModal: () => void;
   onOpenLoginModal: () => void;
   onCloseLoginModal: () => void;
+  onOpenRentModal: () => void;
+  onCloseRentModal: () => void;
 }
--- app/store/store.ts
+++ app/store/store.ts
@@ -5,8 +5,11 @@ import { Store } from "../interfaces";
 export const useStore = create<Store>((set) => ({
   isOpenRegisterModal: false,
   isOpenLoginModal: false,
+  isOpenRentModal: false,
   onOpenRegisterModal: () => set({ isOpenRegisterModal: true }),
   onCloseRegisterModal: () => set({ isOpenRegisterModal: false }),
   onOpenLoginModal: () => set({ isOpenLoginModal: true }),
   onCloseLoginModal: () => set({ isOpenLoginModal: false }),
+  onOpenRentModal: () => set({ isOpenRentModal: true }),
+  onCloseRentModal: () => set({ isOpenRentModal: false }),
 }));
