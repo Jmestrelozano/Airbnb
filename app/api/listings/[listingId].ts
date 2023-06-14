fe03777019cfdf15adb3a8bdde58d4ba29b70ce3
Jmestrelozano <jmestrelozano@gmail.com>
2023-06-14 15:13:14 -0500

    creating the respective endpoints to create the cabin rental

---
+++ app/api/listings/[listingId].ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+
+import getCurrentUser from "@/app/actions/dbUser";
+import prismadb from "@/app/api/db/prismadb";
+
+interface IParams {
+  listingId?: string;
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: IParams }
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  const listing = await prismadb.listing.deleteMany({
+    where: {
+      id: listingId,
+      userId: currentUser.id,
+    },
+  });
+
+  return NextResponse.json(listing);
+}
---
+++ app/api/listings/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+
+import getCurrentUser from "@/app/actions/dbUser";
+import prismadb from "@/app/api/db/prismadb";
+
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const body = await request.json();
+  const {
+    title,
+    description,
+    imageSrc,
+    category,
+    roomCount,
+    bathroomCount,
+    guestCount,
+    location,
+    price,
+  } = body;
+
+  Object.keys(body).forEach((value: any) => {
+    if (!body[value]) {
+      NextResponse.error();
+    }
+  });
+
+  const listing = await prismadb.listing.create({
+    data: {
+      title,
+      description,
+      imageSrc,
+      category,
+      roomCount,
+      bathroomCount,
+      guestCount,
+      locationValue: location.value,
+      price: parseInt(price, 10),
+      userId: currentUser.id,
+    },
+  });
+
+  return NextResponse.json(listing);
+}
