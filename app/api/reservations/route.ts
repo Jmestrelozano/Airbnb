d7ee196e790ba2d56471b7cddc82f018018b2545
Jmestrelozano <jmestrelozano@gmail.com>
2023-06-15 10:31:07 -0500

    creation of the reservations api

---
+++ app/api/reservations/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+
+import prismadb from "@/app/api/db/prismadb";
+import getCurrentUser from "@/app/actions/dbUser";
+
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const body = await request.json();
+  const { listingId, startDate, endDate, totalPrice } = body;
+
+  if (!listingId || !startDate || !endDate || !totalPrice) {
+    return NextResponse.error();
+  }
+
+  const listingAndReservation = await prismadb.listing.update({
+    where: {
+      id: listingId,
+    },
+    data: {
+      reservations: {
+        create: {
+          userId: currentUser.id,
+          startDate,
+          endDate,
+          totalPrice,
+        },
+      },
+    },
+  });
+
+  return NextResponse.json(listingAndReservation);
+}
